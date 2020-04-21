338b5d16eb000c21f12d48edde7a7f80cfeb16a9
raflichs <raflichs@gmail.com>
2020-04-21 10:32:47 +0700

    initial commit

---
+++ kontak/js/script.js
@@ -0,0 +1,13 @@
+//event pada link
+$('.page-scroll').on('click',function(){
+	//ambil isi href
+	var tujuan = $('this').attr('href')
+	//tangkap elemen tujuan
+	var elementujuan = $(tujuan);
+	console.log(elementujuan);
+
+	//pindahkan scroll
+	
+	
+
+});
